89118107d0f0e586fc68ee734341b177876a30e3
boomiguy <boomiadmin@flexport>
2020-05-15 14:01:44 -0400

    Updates to BoomiSonarRules

--- cli/scripts/bin/xpathRulesChecker.sh
+++ cli/scripts/bin/xpathRulesChecker.sh
@@ -19,14 +19,14 @@ printReportHead
 h=0
 find "${baseFolder}" -name "*.xml" | while read componentFile
 do
- componentId=$( cat "${componentFile}" | sed -e 's/bns://' | xmllint --xpath "string(/Component/@componentId)" - )
- componentName=$( cat "${componentFile}" | sed -e 's/bns://' | xmllint --xpath "string(/Component/@name)" - )
- componentVersion=$( cat "${componentFile}" | sed -e 's/bns://' | xmllint --xpath "string(/Component/@version)" - )
- componentType=$( cat "${componentFile}" | sed -e 's/bns://' | xmllint --xpath "string(Component/@type)" - ) 
+ componentId=$( cat "${componentFile}" | sed -e 's/bns://g' | xmllint --xpath "string(/Component/@componentId)" - )
+ componentName=$( cat "${componentFile}" | sed -e 's/bns://g' | xmllint --xpath "string(/Component/@name)" - )
+ componentVersion=$( cat "${componentFile}" | sed -e 's/bns://g' | xmllint --xpath "string(/Component/@version)" - )
+ componentType=$( cat "${componentFile}" | sed -e 's/bns://g' | xmllint --xpath "string(Component/@type)" - ) 
  for (( i=1; i<=${rules}; i++ ))
   do 
    xpath="`cat "${sonarRulesFile}" | xmllint -xpath 'profile/rules/rule['$i']/parameters/parameter/key[text()="expression"]/../value/text()' -`"
-   fail=`cat "${componentFile}" | sed -e 's/bns://' | xmllint --xpath "$xpath" - 2> /dev/null | wc -c`
+   fail=`cat "${componentFile}" | sed -e 's/bns://g' | xmllint --xpath "$xpath" - 2> /dev/null | wc -c`
    if [ "${fail}" -gt 0 ];  then
 		 export VIOLATIONS_FOUND="true"
 		 vPriority="`cat "${sonarRulesFile}" | xmllint -xpath 'profile/rules/rule['$i']/priority/text()' -`" ;

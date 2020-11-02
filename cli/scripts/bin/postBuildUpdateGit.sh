56a12b832c1142ae87ab3cc76778a1b0b07d0ffe
Ajay Natarajan <Ajay.Natarajan@dell.com>
2020-11-02 16:35:09 +0000

    Added script to update GIT post build

---
+++ cli/scripts/bin/postBuildUpdateGit.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+# from common
+function inputs {
+     for ARGUMENT in "$@"
+     do
+       KEY=$(echo $ARGUMENT | cut -f1 -d=)
+       VALUE="$(echo $ARGUMENT | cut -f2 -d=)"
+        for i in "${ARGUMENTS[@]}"
+        do
+            # remove all old values of the ARGUMENTS
+            case "$KEY" in
+              $i)  unset ${KEY}; export eval $KEY="${VALUE}" ;;
+              *)
+                esac
+        done
+        
+		for i in "${OPT_ARGUMENTS[@]}"
+        do
+            # remove all old values of the OPTIONAL ARGUMENTS
+            case "$KEY" in
+              $i)  unset ${KEY}; export eval $KEY="${VALUE}" ;;
+              *)
+                esac
+        done
+
+   done
+ }
+ 
+# unset all variables 
+function clean {
+   for i in "${ARGUMENTS[@]}"
+   do
+       unset $i
+   done
+   unset ARGUMENTS OPT_ARGUMENTS 
+}
+
+ARGUMENTS=(GIT_API_URL GIT_AUTH GIT_COMMIT JENKINS_CALLBACK_URL JOB_BASE_NAME STATE)
+inputs "$@"
+#GIT_API_URL=$1
+#GIT_AUTH=$2
+#GIT_COMMIT=$3
+#JENKINS_CALLBACK_URL=$4
+#JOB_BASE_NAME=$5
+JOB_CONTEXT="${JOB_BASE_NAME// /_}"
+#state=$6
+if [ ! -z "${GIT_COMMIT}" ] && [ ! -z "${GIT_API_URL}" ]
+ then
+ echo "Updating status for commit=$GIT_COMMIT with context=${JOB_CONTEXT}"
+ curl -u $GIT_AUTH ${GIT_API_URL}/statuses/$GIT_COMMIT \
+	-H "${h1}" \
+	-H "${h2}" \
+	-X POST \
+	-d "{\"state\": \"${STATE}\",\"context\": \"${JOB_CONTEXT}\", \"description\": \"Jenkins: ${JOB_CONTEXT}\", \"target_url\": \"$JENKINS_CALLBACK_URL\"}"
+	echo "GIT_API is ${GIT_API_URL}"
+ fi
+
+clean
